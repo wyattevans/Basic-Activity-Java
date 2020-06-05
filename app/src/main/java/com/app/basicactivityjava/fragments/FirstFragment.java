6ecbc6c33772fb8e8fd4016adeb34444aab43e16
Wyatt Evans <syattevans@gmail.com>
2020-06-05 00:02:03 -0500

    Added fragments package.

--- app/src/main/java/com/app/basicactivityjava/fragments/FirstFragment.java
+++ app/src/main/java/com/app/basicactivityjava/fragments/FirstFragment.java
@@ -1,4 +1,4 @@
-package com.app.basicactivityjava;
+package com.app.basicactivityjava.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.app.basicactivityjava.R;
 
 public class FirstFragment extends Fragment {
 

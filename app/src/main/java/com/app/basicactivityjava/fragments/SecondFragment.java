0df7e14294d9a47ba12efff08a1bb1a18df0edb3
Wyatt Evans <syattevans@gmail.com>
2020-06-07 00:04:24 -0500

    Removed comments from second fragment.

--- app/src/main/java/com/app/basicactivityjava/fragments/SecondFragment.java
+++ app/src/main/java/com/app/basicactivityjava/fragments/SecondFragment.java
@@ -19,7 +19,6 @@ public class SecondFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 

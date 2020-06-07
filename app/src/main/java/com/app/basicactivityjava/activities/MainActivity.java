b61256236f91ced58e95838334f823138318134c
Wyatt Evans <syattevans@gmail.com>
2020-06-07 00:06:48 -0500

    Added log to on click method of action button in main activity.

--- app/src/main/java/com/app/basicactivityjava/activities/MainActivity.java
+++ app/src/main/java/com/app/basicactivityjava/activities/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(simpleTAG, "Action button clicked.");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }

b5e65fd2f229cc4df9fe5cbe354deebe17ecbb06
Wyatt Evans <syattevans@gmail.com>
2020-06-08 00:19:40 -0500

    Created string resource for log within on click method in main activity.

--- app/src/main/java/com/app/basicactivityjava/activities/MainActivity.java
+++ app/src/main/java/com/app/basicactivityjava/activities/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(simpleTAG, "Action button clicked.");
+                Log.d(simpleTAG, getString(R.string.action_button_clicked));
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }

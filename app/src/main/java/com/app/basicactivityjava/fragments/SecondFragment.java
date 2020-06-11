cd7774e3ec2ab7d3b255fb4a92c4f22d2ef41c3b
Wyatt Evans <syattevans@gmail.com>
2020-06-11 00:14:54 -0500

    Added log for on view created method in second fragment.

--- app/src/main/java/com/app/basicactivityjava/fragments/SecondFragment.java
+++ app/src/main/java/com/app/basicactivityjava/fragments/SecondFragment.java
@@ -26,6 +26,7 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Log.d(simpleTAG, "OnViewCreated method hit.");
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override

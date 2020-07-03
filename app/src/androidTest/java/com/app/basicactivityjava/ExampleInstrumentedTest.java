913ce9c14d05d0436513c9121c6a2916016a11e5
Wyatt Evans <syattevans@gmail.com>
2020-07-03 14:11:57 -0500

    Added log tag.

--- app/src/androidTest/java/com/app/basicactivityjava/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/app/basicactivityjava/ExampleInstrumentedTest.java
@@ -1,9 +1,12 @@
 package com.app.basicactivityjava;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.app.basicactivityjava.activities.MainActivity;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +15,11 @@ import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private final String simpleTAG = ExampleInstrumentedTest.class.getSimpleName();
+
     @Test
     public void useAppContext() {
+        Log.d(simpleTAG, "useAppContent method hit.");
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.app.basicactivityjava", appContext.getPackageName());

caf8d8bff4463579adf1ac405641aced45dd27d9
Wyatt Evans <syattevans@gmail.com>
2020-07-03 14:14:15 -0500

    Added log tag.

--- app/src/test/java/com/app/basicactivityjava/ExampleUnitTest.java
+++ app/src/test/java/com/app/basicactivityjava/ExampleUnitTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ExampleUnitTest {
+    private final String simpleTAG = ExampleUnitTest.class.getSimpleName();
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);

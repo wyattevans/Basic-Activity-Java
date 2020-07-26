634172f41d5a55dbc350fe830637bc8bc95071c7
Wyatt Evans <syattevans@gmail.com>
2020-07-26 00:08:57 -0600

    Removed logs and updated gradle.

--- app/src/main/java/com/app/basicactivityjava/activities/MainActivity.java
+++ app/src/main/java/com/app/basicactivityjava/activities/MainActivity.java
@@ -1,26 +1,22 @@
 package com.app.basicactivityjava.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.app.basicactivityjava.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
-    private final String simpleTAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(simpleTAG, getString(R.string.on_create_method_hit));
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -29,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(simpleTAG, getString(R.string.action_button_clicked));
                 Snackbar.make(view, R.string.replace_own_action, Snackbar.LENGTH_LONG)
                         .setAction(R.string.action, null).show();
             }
@@ -38,14 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        Log.d(simpleTAG, getString(R.string.options_menu_created));
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Log.d(simpleTAG, getString(R.string.options_item_selected));
         int id = item.getItemId();
 
         if (id == R.id.action_settings) return true;
--- app/src/main/java/com/app/basicactivityjava/fragments/FirstFragment.java
+++ app/src/main/java/com/app/basicactivityjava/fragments/FirstFragment.java
@@ -1,7 +1,6 @@
 package com.app.basicactivityjava.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,25 +12,21 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.app.basicactivityjava.R;
 
 public class FirstFragment extends Fragment {
-    private final String simpleTAG = FirstFragment.class.getSimpleName();
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        Log.d(simpleTAG, getString(R.string.on_create_view_method_hit));
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d(simpleTAG, getString(R.string.on_view_created));
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(simpleTAG, getString(R.string.action_button_clicked));
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
